a69064b3e204dd7830da66393c28b8032aafd2ed
Satinder Kaur <sandykaur2008@gmail.com>
2018-06-14 15:11:14 -0400

    working on views and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Piece(models.Model):
-  name = models.CharField()
-  img_url = models.CharField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  img_url = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,12 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    re_path(r'user/(\w+)/$', views.profile, name = 'profile'),
+    re_path(r'^user/(\w+)/$', views.profile, name = 'profile'),
     path('contact/', views.contact, name = 'contact'),
     path('about/', views.about, name = 'about'),
     re_path(r'^([0-9]+)/$', views.detail, name = 'detail'),
     path('edit_profile/', views.edit_profile, name = 'edit_profile'),
-    path('login/', views.login, name = 'login'),
+    path('login/', views.login_view, name = 'login'),
+    path('logout/', views.logout_view, name = 'logout'),
     path('register/', views.register, name = 'register'),
     path('reset_password/', views.reset_password, name = 'reset_password'),
+    path('reset_password_request/', views.reset_password_request, name = 'reset_password request'),
 ]
