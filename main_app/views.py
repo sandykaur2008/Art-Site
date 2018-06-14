29429a565f1955bad2abf374241a05ca589b4225
Satinder Kaur <sandykaur2008@gmail.com>
2018-06-14 15:11:14 -0400

    working on views and models

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from .models import Piece 
-
+from django.contrib.auth import authenticate, login, logout
+# pylint: disable=E1101
 # Create your views here.
+
 def index(request):
-  return render(request, 'index.html')
+  users = User.objects.all()
+  return render(request, 'index.html', {'users': users})
 
 def profile(request, username):
   user = User.objects.get(username=username)
@@ -25,12 +29,19 @@ def detail(request, piece_id):
 def edit_profile(request):
   return render(request, 'edit_profile.html')
 
-def login(request):
+def login_view(request):
   return render(request, 'login.html')
+
+def logout_view(request):
+  logout(request)
+  return redirect('index.html')
 
 def register(request):
   return render(request, 'register.html')
 
 def reset_password(request):
   return render(request, 'reset_password.html')
+
+def reset_password_request(request):
+  return render(request, 'reset_password_request.html')
 
