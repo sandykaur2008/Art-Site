294dca4f82466297091eea904ca1f5029bdfedd4
Satinder Kaur <sandykaur2008@gmail.com>
2018-06-15 20:46:12 -0400

    working on making profile page work

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,11 +7,14 @@ urlpatterns = [
     path('logout/', views.logout_view, name = 'logout'),
     path('register/', views.register, name = 'register'),
     path('', views.index, name = 'index'),
+    path('about/', views.about, name = 'about'),
+    path('contact/', views.contact, name = 'contact'),
+    re_path(r'^user/(\w+)/$', views.profile, name = 'profile'),
 ]
     
-    # re_path(r'^user/(\w+)/$', views.profile, name = 'profile'),
-    # path('contact/', views.contact, name = 'contact'),
-    # path('about/', views.about, name = 'about'),
+   
+    
+
     # re_path(r'^([0-9]+)/$', views.detail, name = 'detail'),
     # path('edit_profile/', views.edit_profile, name = 'edit_profile'),
     # path('reset_password/', views.reset_password, name = 'reset_password'),
