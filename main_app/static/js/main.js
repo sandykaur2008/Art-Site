dec1560bed641067840039c40dbf47e9f8f9beeb
Satinder Kaur <sandykaur2008@gmail.com>
2018-06-27 23:50:55 -0400

    added styling

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -10,3 +10,12 @@ $('button').on('click', function(event){
                     }
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
