c67b7bdeba1bc68f93453cac7e12aadb8c9d99ff
Satinder Kaur <sandykaur2008@gmail.com>
2018-11-14 05:51:11 -0500

    updated settings

--- ArtSite/settings.py
+++ ArtSite/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import environ 
-environ.Env.read_env('.env')
+if os.path.isfile('./.env'): 
+  environ.Env.read_env('.env')
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
