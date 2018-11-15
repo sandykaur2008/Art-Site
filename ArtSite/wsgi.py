81dac0202fcd8cb6d015f1e8b7eac49a5c9fb869
Satinder Kaur <sandykaur2008@gmail.com>
2018-11-14 23:59:13 -0500

    whitenoise

--- ArtSite/wsgi.py
+++ ArtSite/wsgi.py
@@ -10,7 +10,5 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ArtSite.settings")
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
